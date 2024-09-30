7ee37e8db668ee85d04ae4775fadc25850265be0
Andy-Chow-ICD2O1-02 <andy.chow2@student.tdsb.on.ca>
2024-09-30 17:37:18 +0000

    finished assignment

--- PP1_9.py
+++ PP1_9.py
@@ -1,22 +1,33 @@
 
 
 def q1(): 
+    print('"Hello World"')
   #Write Assignment code here
 
 
 def q2(): 
+    word1 = input("Input a word: ")
+    print(word1.lower())
+    print(word1.upper())
+
   #Write Assignment code here
 
 
 def q3(): 
+    word2 = input("Input a word that is at least 5 letters long: ")
+    print(word2[1:4])
   #Write Assignment code here
 
 
 def q4(): 
+    word3 = input("Input a word: ")
+    print(word3.index("o"))
   #Write Assignment code here
 
 
 def q5(): 
+    word4 = input("Input a word: ")
+    print(len(word4))
   #Write Assignment code here
 
 
@@ -24,8 +35,8 @@ def q5():
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
-q3()
-q4()
-q5()
+# q1()
+# q2()
+# q3()
+# q4()
+# q5()
